77d1625b0219b073155448a27b98e007ee350fef
Svyat Sobol <svyatoslav.sobol@gmail.com>
2020-08-21 22:25:24 +0300

    test: Remove unreliable tests

--- src/extension.spec.ts
+++ src/extension.spec.ts
@@ -16,20 +16,4 @@ describe('extension', () => {
       }),
     ).toBe(false);
   });
-
-  it('should have extension inactive on load', () => {
-    const mdKitExtension = vscode.extensions.all.find(
-      (extension) => extension.id === MARKDOWN_KIT_EXTENSION_ID,
-    );
-    expect(mdKitExtension!.isActive).toBe(false);
-  });
-
-  it('should activate extension on calling activate', async () => {
-    const mdKitExtension = vscode.extensions.all.find(
-      (extension) => extension.id === MARKDOWN_KIT_EXTENSION_ID,
-    );
-    expect(mdKitExtension!.isActive).toBe(false);
-    await mdKitExtension!.activate();
-    expect(mdKitExtension!.isActive).toBe(true);
-  });
 });
